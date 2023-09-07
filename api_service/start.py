5bbbddf8bde9f2c225f438bbd885b241771785d5
nastya312 <kaisba00@mail.ru>
2023-09-08 01:16:33 +0300

    add some ctaff

--- api_service/start.py
+++ api_service/start.py
@@ -2,6 +2,6 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:api_service", host="51.250.81.200", port=7001, log_level="info", reload=True, workers=1,
+        "main:api_service", host="0.0.0.0", port=7001, log_level="info", reload=True, workers=1,
         reload_dirs=['/code', '/base'],
     )
